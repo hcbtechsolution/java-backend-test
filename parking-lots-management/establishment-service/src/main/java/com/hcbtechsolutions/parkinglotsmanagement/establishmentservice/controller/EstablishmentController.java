646e093bc08946664af56186f3309a59f5198012
hcb-techsolutions <hcb007@outlook.com.br>
2024-10-16 13:58:29 -0300

    refactor(Docker compose): :recycle: Add Dockerfile e compose

--- parking-lots-management/establishment-service/src/main/java/com/hcbtechsolutions/parkinglotsmanagement/establishmentservice/controller/EstablishmentController.java
+++ parking-lots-management/establishment-service/src/main/java/com/hcbtechsolutions/parkinglotsmanagement/establishmentservice/controller/EstablishmentController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcbtechsolutions.parkinglotsmanagement.establishmentservice.dto.EstablishmentDto;
-import com.hcbtechsolutions.parkinglotsmanagement.establishmentservice.exception.ResourceAlreadyExistsException;
-import com.hcbtechsolutions.parkinglotsmanagement.establishmentservice.exception.ResourceNotFoundException;
 import com.hcbtechsolutions.parkinglotsmanagement.establishmentservice.service.EstablishmentService;
 
 import lombok.AllArgsConstructor;
@@ -33,13 +31,7 @@ public class EstablishmentController {
         consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
         produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<EstablishmentDto> postEstablishment(@RequestBody EstablishmentDto establishment) {
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(service.save(establishment));
-        } catch (ResourceAlreadyExistsException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
-        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(establishment));
     }
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
@@ -49,13 +41,7 @@ public class EstablishmentController {
     
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<EstablishmentDto> getOneEstablishment(@PathVariable UUID id) {
-        try {
-            return ResponseEntity.ok(service.findOne(id));
-        } catch (ResourceNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
-        }
+        return ResponseEntity.ok(service.findOne(id));
     }
 
     @PutMapping(
@@ -64,25 +50,12 @@ public class EstablishmentController {
         produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<EstablishmentDto> putEstablishment(
         @PathVariable UUID id, @RequestBody EstablishmentDto establishment) {
-        try {
-            return ResponseEntity.ok(service.update(id, establishment));
-        } catch (ResourceNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
-        }
+        return ResponseEntity.ok(service.update(id, establishment));
     }
 
     @DeleteMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Void> deleteEstablishment(@PathVariable UUID id) {
-        try {
-            service.delete(id);
-            return ResponseEntity.noContent().build();
-        } catch (ResourceNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
-        }
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
-    
 }
