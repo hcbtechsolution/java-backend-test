e45e16555f26183ebc1ddfe1cf7953cf3e734faa
hcb-techsolutions <hcb007@outlook.com.br>
2024-10-22 09:38:06 -0300

    fix: :bug: Fix Return Status Code

--- parking-lots-management/parking-control-service/src/main/java/com/hcbtechsolutions/parkinglotsmanagement/parkingcontrolservice/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ parking-lots-management/parking-control-service/src/main/java/com/hcbtechsolutions/parkinglotsmanagement/parkingcontrolservice/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,29 +11,40 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.hcbtechsolutions.parkinglotsmanagement.parkingcontrolservice.dto.ExceptionDto;
+import com.hcbtechsolutions.parkinglotsmanagement.parkingcontrolservice.exception.ResourceAlreadyExistsException;
 import com.hcbtechsolutions.parkinglotsmanagement.parkingcontrolservice.exception.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+	@ExceptionHandler(Exception.class)
 	public final ResponseEntity<ExceptionDto> handleAllExceptions(Exception ex, WebRequest request) {
 		ExceptionDto exception = new ExceptionDto(
 				new Date(),
 				ex.getMessage(),
 				request.getDescription(false));
-		
+
 		return new ResponseEntity<>(exception, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
+
+	@ExceptionHandler(ResourceAlreadyExistsException.class)
+	public final ResponseEntity<ExceptionDto> handleResourceAlreadyExistsException(Exception ex, WebRequest request) {
+		ExceptionDto exception = new ExceptionDto(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(exception, HttpStatus.CONFLICT);
+	}
+
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public final ResponseEntity<ExceptionDto> handleResourceNotFoundException(Exception ex, WebRequest request) {
 		ExceptionDto exception = new ExceptionDto(
 				new Date(),
 				ex.getMessage(),
 				request.getDescription(false));
-		
+
 		return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
 	}
 }
